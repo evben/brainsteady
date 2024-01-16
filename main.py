d2752b4027b595201a91f89e3b23f268716d58db
Eva Benoit <91141868+evben@users.noreply.github.com>
2024-01-16 19:44:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from bottle import route, post, install, template, request, run
+from bottle_sqlite import SQLitePlugin
+
+"""
+Connexion à la base de données SQLite
+"""
+install(SQLitePlugin(dbfile='classe.db'))
+
+
+# Routes de l'application
+@route('/')
+def index():
+    """ Page d'accueil """
+    return template('templates/index.tpl')
+
+
+"""
+On ouvre un serveur en local sur le port 7000
+http://localhost:7000/ dans le navigateur pour accéder au site après
+avoir exécuté ce programme Python.
+"""
+if __name__ == "__main__":
+    run(reloader=True, debug=True, host="127.0.0.1", port=7000)
